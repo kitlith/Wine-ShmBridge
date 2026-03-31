42694022eb2ad3b2051645bb706afc628937d70a
Kitlith <kitlith@kitl.pw>
2026-03-30 18:25:41 -0700

    Fix build for x86_64
    untested, committing so I can switch over to my arch install

--- Build.sh
+++ Build.sh
@@ -9,4 +9,5 @@ if [ ${#LIB_VERSION} -lt 5 ]; then
     LIB_VERSION="0.0.1"
 fi
 
-winegcc -shared -lrt -mno-cygwin -O3 -DVERSION_INFO="\"$LIB_VERSION\"" version.c shmbridge.c shmbridge.spec -o shmbridge.dll
+clang shmbridge.c -O3 -shared -o shmbridge.so -I /usr/include/wine/windows/ -L /usr/lib64/wine-wow64/wine/x86_64-unix/ -l:ntdll.so -lrt
+winegcc -shared -lrt -mno-cygwin -O3 -DVERSION_INFO="\"$LIB_VERSION\"" version.c winshim.c shmbridge.spec -o shmbridge.dll
--- winshim.c
+++ winshim.c
@@ -1,6 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
-#include <unixlib.h>
+#include <wine/unixlib.h>
 #include "shmbridge.h"
 
 unixlib_handle_t __shmbridge_unixlib_handle;
